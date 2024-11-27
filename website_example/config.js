1f936abacf521872439a40ed05d9ffb6a28a0880
SuperBlockchain-Pool <55582476+SuperBlockchain-Pool@users.noreply.github.com>
2024-11-27 17:08:47 +0100

    v2.6.0

--- website_example/config.js
+++ website_example/config.js
@@ -1,18 +1,14 @@
-var api = "https://your-api.pool.com/apiMerged";
-var apiMerged = "https://your-api.pool.com/apiMerged1";
-let parentCoin = "nameCoin"
+var api = "https://your-api.pool.com/api";
+let parentCoin = "nameCoin";
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
-var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+var email = "";
+var telegram = "";
+var discord = "";
+var facebook = "";
+var twitter = "";
+var instagram = "";
 
-var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
-
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USDT"];
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
